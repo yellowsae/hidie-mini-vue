4d15acf1209f16ca121f5da1b19a191fc7665546
yellowsea <yunhai0644@163.com>
2022-08-07 14:52:02 +0800

    diff算法 - 左侧一样 新的比老的长, 创建节点逻辑

--- example/update/patchChildren/ArrayToArray.js
+++ example/update/patchChildren/ArrayToArray.js
@@ -67,23 +67,29 @@ import { h, ref } from "../../../lib/guide-mini-vue.esm.js"
 
 
 
+
+
+
+
+
 // 2.右侧对比
 //  A ( B  C )
 //  D E (B C )
-const prevChildren = [
-  // 多谢了一个 Key
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "C" }, "C"),
-];
+// const prevChildren = [
+//   // 多谢了一个 Key
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
 
-const nextChildren = [
-  h("p", { key: "D" }, "D"),
-  h("p", { key: "E" }, "E"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "C" }, "C"),
-];
+// const nextChildren = [
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
 /**
+ * 右侧对比 实现思路：
  * 1. 基于指针 i  e1  e2
  * 
  * 2. 循环 n2 n2 对比 
@@ -95,6 +101,122 @@ const nextChildren = [
  * - 这里 右侧对比 为 删除 A ，创建 D E 节点
  * - 基于指针的判断 i <= e1 && i <= e2
  */
+
+
+
+
+
+
+
+
+
+// 对比场景
+// 3 左侧一样， 新的比老的长  -> 创建节点
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+// ];
+
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+
+/**
+ * 1. 左侧一样， 新的比老的长 
+ *  - Array1: A B 
+ *  - Array2: A B C D
+ *  - 基于左侧对比，得到变化的元素，创建 C ， 把新创建的 C 添加到尾部
+ * 
+ * 实现对比-> 创建 C 节点 
+ * 1. 两个Array 进行对比， 因为是左侧一样， 新的比老的长，多出了 C 节点
+ * 2. 基于指针 i  e1  e2 , 确定 C节点的位置 
+ * 3. 创建 C 节点 添加到 Array2 尾部 
+ * 
+ *    - 最后指针为 i = 1  |  e1 = 1 | e2 = 2  
+ *    - 创建 C 节点，添加到 Array2 尾部
+ *    - 所以 i > e1  && i <= e2 时创建节点
+ * 
+ */
+
+
+
+
+
+
+
+
+
+// 4. 右侧一样， 新的比老的长
+const prevChildren = [
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+];
+
+const nextChildren = [
+  h("p", { key: "C" }, "C"),
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+];
+/**
+ *  2. 右侧一样， 新的比老的长
+ *  - Array1: A B
+ *  - Array2: C A B
+ *  - 基于右侧对比，得到变化的元素， 创建 C ，把新创建的 C 添加到头部
+ * 
+ * 实现对比-> 创建 C 节点
+ *  1. 两个Array 进行对比，基于右侧对比， 因为是右侧一样， 新的比老的长，多出了 C 节点
+ *  2. 基于指针 i  e1  e2 , 确定 C节点的位置
+ *  3. 创建 C 节点 添加到 Array2 头部
+ * 
+ *  - 右侧进行对比完，最后的指针为 i = 0 | e1 = -1 | e2 = 0
+ *  - 所以在 i > e1  && i <= e2 时 创建新的节点 
+ *  - 创建 C 节点，添加到 Array2 头部 ， 指定的位置
+ *      - 实现添加到头部需要 ： 锚点 -> 基于锚点 把创建的节点添加锚点的前一位置 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 对比场景 
+ *  1. 左侧一样， 新的比老的长 
+ *  - Array1: A B 
+ *  - Array2: A B C D
+ *  - 基于左侧对比，得到变化的元素，创建 C ， 把新创建的 C 添加到尾部
+ * 
+ * 
+ * 2. 右侧一样， 新的比老的长
+ *  - Array1: A B
+ *  - Array2: C A B
+ *  - 基于右侧对比，得到变化的元素， 创建 C ，把新创建的 C 添加到头部
+ * 
+ * 
+ * 3. Array1 比 Array2 长
+ *  - Array1: A B C 
+ *  - Array2: A B
+ *  - 基于左侧对比，  找到乱序元素 C ,  执行删除 C 
+ * 
+ * 4. Array1 比 Array2 长
+ *  - Array1: A B C
+ *  - Array2: B C
+ *  - 基于左侧对比， 得到变化的元素 A ，  执行删除 A 
+ */
+
+
 
 export default {
   name: "ArrayToArray",
@@ -114,36 +236,6 @@ export default {
   }
 };
 
-
-
-
-/**
- * 对比场景 
- * 
- * 1. 左侧一样， 新的比老的长 
- *  - Array1: A B 
- *  - Array2: A B C
- *  - 基于左侧对比，得到变化的元素，创建 C ， 把新创建的 C 添加到尾部
- * 
- * 
- * 2. 右侧一样， 新的比老的长
- *  - Array1: A B
- *  - Array2: C A B
- *  - 基于右侧对比，得到变化的元素， 创建 C ，把新创建的 C 添加到头部
- * 
- * 
- * 3. Array1 比 Array2 长
- *  - Array1: A B C 
- *  - Array2: A B
- *  - 基于左侧对比，  找到乱序元素 C ,  执行删除 C 
- * 
- * 4. Array1 比 Array2 长
- *  - Array1: A B C
- *  - Array2: B C
- *  - 基于左侧对比， 得到变化的元素 A ，  执行删除 A 
- * 
- * 
- */
 
 
 /**
