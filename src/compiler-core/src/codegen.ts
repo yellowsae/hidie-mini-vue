0e598d9d72865b0248c51c6be48602c683a4ddfb
yellowsea <yunhai0644@163.com>
2022-09-11 21:53:24 +0800

    重构

--- src/compiler-core/src/codegen.ts
+++ src/compiler-core/src/codegen.ts
@@ -5,9 +5,17 @@ export function generate(ast) {
 
   // 实现根据 ast 生成的 render 函数
 
+  // 重构 
+  // - code 过多 
+  // - += 过多
+  // 实现一个全局上下文对象 保存 code
+  const context = createCodegenContext()
+  const { push } = context
+
   // 定义code -> 最终返回 
-  let code = ""
-  code += 'return '
+  // let code = ""
+  // code += 'return '
+  push('return ')
 
   // 函数名
   const functionName = "render"
@@ -17,25 +25,31 @@ export function generate(ast) {
   // 把 array 转为 string 
   const signature = args.join(', ')
 
-  // 拼接函数
-  code += `function ${functionName}(${signature}) {`
-  // 函数体的内容 - 基于 ast 实现 
-  code += `return `
-  // const node = ast.children[0]
-  // 改为由 transform 提供的根节点 - 而不是 直接 取出 ast.children[0]
-  // 抽离 返回内容的逻辑 为一个函数 
-  code = genNode(code, ast)
-  // const node = ast.codegenNode
-  // code += `'${node.content}'`
 
-  // 尾部 }
-  code += '}'
+  // 重构 
+  push(`function ${functionName}(${signature}) {`)
+  push(`return `)
+  genNode(context, ast.codegenNode)
+  push("}")
+  // // 拼接函数
+  // code += `function ${functionName}(${signature}) {`
+  // // 函数体的内容 - 基于 ast 实现 
+  // code += `return `
+  // // const node = ast.children[0]
+  // // 改为由 transform 提供的根节点 - 而不是 直接 取出 ast.children[0]
+  // // 抽离 返回内容的逻辑 为一个函数 
+  // code = genNode(code, ast)
+  // // const node = ast.codegenNode
+  // // code += `'${node.content}'`
+
+  // // 尾部 }
+  // code += '}'
 
 
 
   // 返回 code
   return {
-    code
+    code: context.code
   }
 
   // 伪实现 
@@ -51,8 +65,29 @@ export function generate(ast) {
 
 
 // 抽离 render 返回内容的逻辑 
-function genNode(code, ast) {
-  const node = ast.codegenNode
-  code += `'${node.content}'`
-  return code
+// function genNode(code, ast) {
+//   const node = ast.codegenNode
+//   code += `'${node.content}'`
+//   return code
+// }
+// 重构
+function genNode(context, node) {
+  const { push } = context
+  push(`'${node.content}'`)
 }
+
+
+// 创建 基于 code 的全局上下文对象
+function createCodegenContext() {
+  const context = {
+    // 存入 code
+    code: '',
+    // += 的行为 
+    push(source) {
+      context.code += source
+    }
+  }
+
+  return context
+}
+
